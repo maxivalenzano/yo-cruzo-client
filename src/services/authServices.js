979328a7ffd84330f9c2a72ae22c286076be7407
MaxiValenzano <maxivalenzano@gmail.com>
2022-08-09 13:27:04 -0300

    se eliminan console.log

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,22 +1,8 @@
 import axios from '../axios/axios';
 
-const signIn = (data) => {
-  console.log('🚀 ~ file: authServices.js ~ line 4 ~ signIn ~ data', data);
-  return axios.post('/api/auth/signin', data).then((response) => response.data);
-  // .catch((err) => {
-  //   console.error(err);
-  //   return err.response;
-  // });
-};
+const signIn = (data) => axios.post('/api/auth/signin', data).then((response) => response.data);
 
-const signUp = (data) => {
-  console.log('🚀 ~ file: authServices.js ~ line 14 ~ signUp ~ data', data);
-  return axios.post('/api/auth/signup', data).then((response) => response.data);
-  // .catch((err) => {
-  //   console.error(err);
-  //   return err.response;
-  // });
-};
+const signUp = (data) => axios.post('/api/auth/signup', data).then((response) => response.data);
 
 const authServices = {
   signIn,
